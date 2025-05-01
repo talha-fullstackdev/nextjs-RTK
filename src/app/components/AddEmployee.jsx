bf417c0ed31d8c748bfb915a95c70b588dde4759
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-01 13:17:55 +0500

    made showemployee and addemployee component

---
+++ src/app/components/AddEmployee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+    <div>
+        <input type="text" placeholder='add employee data' />
+        <button>add</button>
+    </div>
+  )
+}
+
+export default AddEmployee
---
+++ src/app/components/ShowEmployee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ShowEmployee = () => {
+  return (
+    <div>
+        <h2>show employees data</h2>
+
+    </div>
+  )
+}
+
+export default ShowEmployee
