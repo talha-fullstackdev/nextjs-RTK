5b1cd1a6b68930de99300bb4a3586b823a12641b
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-02 11:09:22 +0500

    updaated

--- src/app/components/ShowEmployee.jsx
+++ src/app/components/ShowEmployee.jsx
@@ -1,19 +1,20 @@
-"use client"
-import React from 'react'
-import { useSelector } from 'react-redux'
+"use client";
+import React from "react";
+import { useSelector } from "react-redux";
 const ShowEmployee = () => {
-    const employeeData = useSelector((data)=>data.employees)
+  const employeeData = useSelector((data) => data.employees);
 
   return (
     <div>
-        <h2>{employeeData.map((item,i)=>(
-            <div className="" key={i}>
-                <h1>{item.name}</h1>
-            </div>
-        ))}</h2>
-
+      <h2>
+        {employeeData.map((item, i) => (
+          <div className="" key={i}>
+            <h1>{item.name}</h1>
+          </div>
+        ))}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default ShowEmployee
+export default ShowEmployee;
