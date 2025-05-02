f74c3ee9a5832dbcb41d0f8f5ec91cd087ef7fea
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-02 17:37:27 +0500

    updated from terminal

--- src/app/components/ShowEmployee.jsx
+++ src/app/components/ShowEmployee.jsx
@@ -29,4 +29,3 @@ const ShowEmployee = () => {
 };
 
 export default ShowEmployee;
-
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
