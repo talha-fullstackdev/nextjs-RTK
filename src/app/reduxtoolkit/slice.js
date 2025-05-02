c4f034dc78fd63152bee1a8b32e452bc0a92bb83
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-02 11:10:12 +0500

    updated

--- src/app/reduxtoolkit/slice.js
+++ src/app/reduxtoolkit/slice.js
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 const initialState = {
   employees: [],
 };
 const Slice = createSlice({
-  name:"addSlice",
+  name: "addSlice",
   initialState,
   reducers: {
     add: (state, action) => {
-        console.log(action)
+      console.log(action);
       const data = {
         id: nanoid(),
         name: action.payload,
--- src/app/reduxtoolkit/store.js
+++ src/app/reduxtoolkit/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./slice";
 const store = configureStore({
-      reducer
-})
-export default store
+  reducer,
+});
+export default store;
