12f2da7638f0ef9a196e0e35c9220f9501d5d96f
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-02 11:35:12 +0500

    add deleted employee functionality in redux toolkit

--- src/app/components/ShowEmployee.jsx
+++ src/app/components/ShowEmployee.jsx
@@ -1,15 +1,27 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 const ShowEmployee = () => {
+  const [data, setData] = useState([]);
   const employeeData = useSelector((data) => data.employees);
+  console.log(data)
+  const handleDelete = (id) => {
+    const filterData = data.filter((item) => item.id !== id);
+    setData(filterData);
+    return alert(`deleted succesfully `)
+
+  };
+  useEffect(() => {
+    setData(employeeData);
+  }, [employeeData]);
 
   return (
     <div>
       <h2>
-        {employeeData.map((item, i) => (
+        {data.map((item, i) => (
           <div className="" key={i}>
             <h1>{item.name}</h1>
+            <button onClick={() => handleDelete(item.id)}>delete</button>
           </div>
         ))}
       </h2>
